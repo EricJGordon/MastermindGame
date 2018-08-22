fea20bca186d93d6b64d93370dbcae7e16a052d8
Eric Gordon <eric.gordon@ucdconnect.ie>
2018-08-22 02:20:37 +0100

    For Prob538, creating new algorithm to be more efficient than original one which tested all cases every time unnecessarily.

--- Misc/main.c
+++ Misc/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <stdbool.h>
 
 
 double twoDecs(double n)
@@ -11,7 +12,7 @@ double twoDecs(double n)
 
 char* Prob538(double target, int n)
 {
-    int i, j, closesti = 1, closestj = 1;
+    int i, j, closesti = 1, closestj = 1, iter = 0;
     double closest = 1;
     target/=100;        // to put in range 0 to 1
 
@@ -22,18 +23,79 @@ char* Prob538(double target, int n)
         for(j=1;j<i;j++)
         {
             //TODO: remove checking redundant fractions like 2/4, 3/9 by checking if mod 1 of i/j (yes i/j, not j/i) != 0
+            printf("\n %d/%d", j, i);
+
 
             if(fabs(target - (double)j/i) < fabs(target - closest))
             {
-             //     printf("\n%d/%d", j, i);
-             closestj = j;
-             closesti = i;
-             closest = (double)j/i;
+                printf("(New closest)");
+
+                closestj = j;
+                closesti = i;
+                closest = (double)j/i;
             }
 
+            iter++;
+        }
+    }
+
+    printf("\nOriginal version took %d iterations  (%d in %d odds) \n", iter, closestj, closesti);
+    iter=0;
+
+    j=1;
+    bool needsDecr = false, higher = false;      //higher referring to the *target* value being higher than the current one
+    double diff;
+
+    for(i=1;i<=n;i++)
+    {
+        printf("\nOuter loop: %d/%d", j, i);
+
+        diff = target - (double)j/i;
+
+        higher = diff > 0 ? true : false;
+
+        if (fabs(diff) < fabs(target - closest))
+        {
+            closestj = j;
+            closesti = i;
+            closest = (double) j / i;
         }
 
+
+
+
+
+        for(;higher;)
+        {
+           j++;        //here instead of end of loop because even when it already went over the target, it would still increment
+                       //meaning it would always increment one too many times
+
+            printf("\nInner Loop: %d/%d", j, i);
+
+            diff = target - (double)j/i;
+
+            higher = diff > 0 ? true : false;
+
+            if(fabs(target - (double)j/i) < fabs(target - closest))
+            {
+                closestj = j;
+                closesti = i;
+                closest = (double)j/i;
+            }
+
+            needsDecr = true;
+            iter++;
+        }                   //can't currently guarantee same outcome as original inefficient version.
+                        //TODO: Create a check for that then run so many tests
+
+        //if(needsDecr)
+          //  j--;
+
+        iter++;
+
     }
+
+    printf("\nAlternative  version took %d iterations\n", iter);
 
 
     unsigned int buffersize = 13 ;
@@ -51,15 +113,15 @@ int main(void)
 
     while(getchar())        //as opposed to running the whole program multiple times because then it wasn't random enough
     {
-        int num = rand();
+        /*int num = rand();
 
         printf("\nTime = %d", (unsigned int)time(NULL));
         printf("\nRand Num: %d", num);
 
         double newNum = (double) (num % 10000) / 100;       //to put in form xx.yy  e.g. 73.56%
-        printf("\n%.2lf%%", newNum);
+        printf("\n%.2lf%%", newNum);*/
 
-        printf("\n%s \n", Prob538(newNum, 10));
+        printf("\n%s \n", Prob538(33.56, 10));
     }
     return 0;
 }
